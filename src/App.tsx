98742adb60cf642ddc3d8be0898cbbb7dadbd5bd
syntaxloom <pkehinde27@gmail.com>
2025-03-06 18:32:43 +0100

    improved the boilerplate to include react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import "./App.css"
+import Home from "./pages/home/index"
+import About from "./pages/about/index"
+import Contact from "./pages/contact/index"
 
 function App() {
   return (
-    <div className="p-4 text-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Hello Tailwind!
-      </h1>
-    </div>
+    <Router>
+      <div className="min-h-screen">
+        {/* Navigation */}
+        <nav className="bg-gray-100 p-4">
+          <ul className="flex space-x-4 justify-center">
+            <li>
+              <Link to="/" className="text-blue-600 hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="text-blue-600 hover:underline">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-blue-600 hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Page Content */}
+        <main className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <div className="p-4 text-center">
+            <h1 className="text-3xl font-bold text-blue-600">Contact Page</h1>
+            <p className="mt-4">Get in touch with us here.</p>
+        </div>
+    )
+}
+
