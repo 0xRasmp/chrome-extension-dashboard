56dda149438627c50645b03d4a5669015c7fb282
syntaxloom <pkehinde27@gmail.com>
2025-03-06 18:33:31 +0100

    improved the boilerplate to include react-router-dom

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,8 @@
 export default function () {
     return (
         <div className="p-4 text-center">
-            <h1 className="text-3xl font-bold text-blue-600">Contact Page</h1>
-            <p className="mt-4">Get in touch with us here.</p>
+            <h1 className="text-3xl font-bold text-blue-600">Home page</h1>
+            <p className="mt-4">This is the home page of our application.</p>
         </div>
     )
 }
